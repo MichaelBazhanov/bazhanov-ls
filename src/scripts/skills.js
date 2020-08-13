fe7a68f751f7c83538fbfb71c09c9e3e0b78a059
michael bazhanov <michaelbazhanov33@gmail.com>
2020-08-13 19:52:38 +0300

    add vue skills

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,76 @@
-console.log('this is the skills module');
+import Vue from "vue";
+
+const skillsItem = {
+	props: ['skill'],
+	template: '#skills-item',
+	methods: {
+		drawPersentageCircle() {
+			//если нужен доступ до дом элементов
+			const circle = this.$refs['colored-circle'];
+			// circle - это svg круг
+			// stroke-dasharray - это css свойство
+			const dashArray = parseInt(
+				getComputedStyle(circle).getPropertyValue('stroke-dasharray')
+			);
+			const percent = (dashArray / 100) * (100 - this.skill.percent);
+			//              (получили 1 %)      (из 100% вычитаем столько сколько пришло и на разницу закрашиваем)
+			circle.style.strokeDashoffset = percent;
+		}
+	},
+	mounted() {
+		//всю логику стараемся перекинуть в методы
+		this.drawPersentageCircle();
+	},
+}
+
+const skillsRow = {
+	props: ['category'],
+	template: '#skills-row',
+	components: {
+		skillsItem: skillsItem,
+	}
+}
+
+new Vue({
+	el: '#skills-component',
+	template: '#skills-list',
+	components: {
+		skillsRow: skillsRow,
+	},
+	data() {
+		return {
+			skills: [],
+		}
+	},
+	created() {
+		//если не нужен доступ до дом элементов
+		this.skills = require('../data/skills.json'); //json to array
+	},
+	mounted() {
+		//если нужен доступ до дом элементов
+
+	},
+})
+// /---------------------
+// new Vue({
+// 	el: '#skills-component',
+// 	template: '#skills-list',
+// 	components: {
+// 		skillsRow: {
+// 			template: '#skills-row',
+// 			components: {
+// 				skillsItem: {
+// 					template: '#skills-item',
+// 					data() {
+// 						return {
+// 							title: 'Заголовок'
+// 						}
+// 					},
+// 					mounted() {
+// 						this.title = this.title + '1234567890'
+// 					}
+// 				},
+// 			}
+// 		},
+// 	}
+// })
