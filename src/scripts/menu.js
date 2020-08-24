0bb855450d77505a3c39968f6cde61653fa0bed9
michael bazhanov <michaelbazhanov33@gmail.com>
2020-08-24 19:20:41 +0300

    add adoptiv menu

---
+++ src/scripts/menu.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+new Vue({
+	el: '#menu-component',
+	template: `#menu-container`,
+	data() {
+		return {
+
+		}
+	},
+	computed: {
+		burger() {
+			let burger = document.querySelector('.burger-menu');
+			burger.addEventListener('click', (e) => {
+				e.target.closest('.burger-menu').classList.toggle('active-burger');
+
+				let nav = this.$refs['navigation'];
+				nav.classList.toggle('active-nav');
+			});
+
+		}
+	},
+	created() {
+		console.log('created')
+	},
+	mounted() {
+		console.log(this.burger)
+	}
+})
