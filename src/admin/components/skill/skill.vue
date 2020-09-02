b203073de8590f37d763367ea75dca778f671da8
michael bazhanov <michaelbazhanov33@gmail.com>
2020-09-02 18:55:29 +0300

    переделал валидацию skill

--- src/admin/components/skill/skill.vue
+++ src/admin/components/skill/skill.vue
@@ -11,19 +11,19 @@
 	<!-- отображение при изменение скила -->
 	<div class="skill-component" v-else>
 		<div class="title">
+				<!-- @input="onClick1()" -->
+				<!-- :errorMessage="errorMessageSkill" -->
 			<app-input
-				@input="onClick1()"
-				:errorMessage="errorMessageSkill"
-
+				:errorMessage="validation.firstError('currentSkill.title')"
 				v-model="currentSkill.title"
 				noSidePaddings
 			/>
 		</div>
 		<div class="percent">
+				<!-- @input="onClick2()" -->
+				<!-- :errorMessage="errorMessagePercent" -->
 			<app-input
-				@input="onClick2()"
-				:errorMessage="errorMessagePercent"
-
+				:errorMessage="validation.firstError('currentSkill.percent')"
 				v-model="currentSkill.percent"
 				type="number"
 				min="0"
@@ -32,7 +32,7 @@
 			/>
 		</div>
 		<div class="buttons">
-			<icon symbol="tick" class="btn" @click="$emit('approve', currentSkill )"/>
+			<icon symbol="tick" class="btn" @click="handleInput"/>
 			<icon symbol="cross" class="btn" @click="currentSkill.editmode = false"/>
 		</div>
 	</div>
@@ -41,8 +41,21 @@
 <script>
 import icon from "../icon";
 import input from "../input";
+import { Validator, mixin as ValidatorMixin } from "simple-vue-validator";
 
 export default {
+	mixins: [ValidatorMixin],
+	validators: {
+		"currentSkill.title": value => {
+			return Validator.value(value).required("Не может быть пустым")
+		},
+		"currentSkill.percent": value => {
+			return Validator.value(value)
+				.integer("Должен быть числом")
+				.between(0, 100, "Некорректное значение")
+				.required("Не может быть пустым")
+		}
+	},
 	props: {
 		skill: {
 			type: Object,
@@ -53,12 +66,12 @@ export default {
 	data() {
 		return {
 			// skill: currentSkill.title,
-			validSkill: false,
-			errorMessageSkill: '',
+			// validSkill: false,
+			// errorMessageSkill: '',
 
 			// percent: currentSkill.percent,
-			validPercent: false,
-			errorMessagePercent: '',
+			// validPercent: false,
+			// errorMessagePercent: '',
 
 			// editmode: false,
 			currentSkill: {
@@ -76,29 +89,34 @@ export default {
 
 	},
 	methods: {
-		onClick1() {
-			let s = this.currentSkill.title.trim();
-			if ( s === "") {
-				this.validSkill = false;
-				this.errorMessageSkill = 'Пустая строка';
-			} else {
-				this.validSkill = true;
-				this.errorMessageSkill = '';
-			}
-		},
-		onClick2() {
-			let p = this.currentSkill.percent.trim();
-			if (p === "" ||
-				p > 100  ||
-				p <  0
-				) {
-				this.validPercent = false;
-				this.errorMessagePercent = 'Процент не определен или неверен';
-			} else {
-				this.validPercent = true;
-				this.errorMessagePercent = '';
-			}
-		},
+		// onClick1() {
+		// 	let s = this.currentSkill.title.trim();
+		// 	if ( s === "") {
+		// 		this.validSkill = false;
+		// 		this.errorMessageSkill = 'Пустая строка -------------';
+		// 	} else {
+		// 		this.validSkill = true;
+		// 		this.errorMessageSkill = '';
+		// 	}
+		// },
+		// onClick2() {
+		// 	let p = this.currentSkill.percent.trim();
+		// 	if (p === "" ||
+		// 		p > 100  ||
+		// 		p <  0
+		// 		) {
+		// 		this.validPercent = false;
+		// 		this.errorMessagePercent = 'Процент не определен или неверен ------------------';
+		// 	} else {
+		// 		this.validPercent = true;
+		// 		this.errorMessagePercent = '';
+		// 	}
+		// },
+		async handleInput() {
+			if( await this.$validate() == false) return
+			this.$emit('approve', this.currentSkill)
+			// @click="$emit('approve', currentSkill )"
+		}
 	}
 }
 </script>
