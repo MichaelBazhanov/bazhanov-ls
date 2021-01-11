c76d08008c918c3062f68138e43e8c580e06567f
michael bazhanov <michaelbazhanov33@gmail.com>
2021-01-11 18:22:33 +0300

    fix

--- src/admin/router.js
+++ src/admin/router.js
@@ -68,26 +68,48 @@ router.beforeEach( async (to, from, next) => {
 	const isPublicRoute = to.matched.some( record => record.meta.public);//определяем публичный/не публичный путь по свойству meta в объектах routes
 	const token = localStorage.getItem('token');//получаем токен авторизации
 
-	// console.log('isPublicRoute = ',!isPublicRoute)
-	// console.log('token = ',!token)
+	// console.log('isPublicRoute = ',isPublicRoute)
+	// console.log('token = ',token)
 
-	if(!isPublicRoute && !token) { //если маршрут не публичный и нет токена
+	if(isPublicRoute) { //если маршрут публичный
+		localStorage.removeItem('token') //удаляем токен - РАЗЛОГИНИВАЕМСЯ
+		next(); //разрешает переход
+
+	} else { //если маршрут не публичный и есть токен (работоспособность token'a будит проверена на запросе /user )
 		$axios.defaults.headers["Authorization"] = `Bearer ${token}`; //в заголовок ставим сохраненный token, если он не действительный то
 		//  запрос /user вылетает на catch()
 
 		try {
 			const response = await $axios.get("https://webdev-api.loftschool.com/user");
-			localStorage.setItem('token', response.data.token);//устанавливаем токен
 			next(); //разрешает переход
 
 		} catch(e) {//здесь нас РАЗЛОГИНИВАЕТ (те в axios в заголовке нету действующего token при обращении к серверу)
 			// console.error(e, 'not authorized')
-			localStorage.removeItem('token') //удаляем токен
-			// router.replace('/login') // или можно next('/')
-			next('/')
-		}
+			localStorage.removeItem('token') //удаляем токен - РАЗЛОГИНИВАЕМСЯ
+			next('/login')
 
-	} else {
-		next(); //разрешает переход
+		}
 	}
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// if(!isPublicRoute && !token) { //если маршрут не публичный и нет токена
+	// 	$axios.defaults.headers["Authorization"] = `Bearer ${token}`; //в заголовок ставим сохраненный token, если он не действительный то
+	// 	//  запрос /user вылетает на catch()
+
+	// 	try {
+	// 		const response = await $axios.get("https://webdev-api.loftschool.com/user");
+	// 		localStorage.setItem('token', response.data.token);//устанавливаем токен
+	// 		next(); //разрешает переход
+
+	// 	} catch(e) {//здесь нас РАЗЛОГИНИВАЕТ (те в axios в заголовке нету действующего token при обращении к серверу)
+	// 		// console.error(e, 'not authorized')
+	// 		localStorage.removeItem('token') //удаляем токен
+	// 		// router.replace('/login') // или можно next('/')
+	// 		next('/')
+	// 	}
+
+	// } else {
+	// 	next(); //разрешает переход
+	// }
 })
