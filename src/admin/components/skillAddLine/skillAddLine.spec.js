d2a9107c812776318b3fe49ac4fc0cad02c2ddf2
michael bazhanov <michaelbazhanov33@gmail.com>
2021-01-30 21:10:48 +0300

    fix

--- src/admin/components/skillAddLine/skillAddLine.spec.js
+++ src/admin/components/skillAddLine/skillAddLine.spec.js
@@ -1,9 +1,9 @@
 import skillAddLine from "./skillAddLine"
-import input from "../input"
 import roundBtn from "../button/types/roundBtn";
+import button from "../button";
 import { mount, shallowMount } from "@vue/test-utils"
 
-it('Проверка на заблокированный skillAddLine ', async () => {
+it('Проверка на разблокированный skillAddLine emit c button', async () => {
 	//steps
 	const wrapper = mount(skillAddLine, {
 		propsData: { //устанавливаем props
@@ -21,17 +21,33 @@ it('Проверка на заблокированный skillAddLine ', async (
 
 	// //actions
 	// await wrapper.setProps({blocked: Boolean(true)}) //блокировка
-	const btnRound = wrapper.findComponent(roundBtn);
-	// console.log(btnRound);
+	const findButton = wrapper.findComponent(button)
+	// const rBtn = findButton.findComponent(roundBtn)
+	// console.log(findButton);
+	// console.log(findButton.vm.$options);
 
-	await btnRound.trigger('click');
-	await wrapper.vm.$nextTick(); //ожидаем асинхронного обновления всего
-	// console.log(btnRound);
-	// btnRound.vm.$emit('approve');
-	// wrapper.vm.$emit('approve')
+
+	// const child = await findButton.vm.$options.components.round();
+	// console.log(child);
+	// const list = findButton.vm.$listeners; //берем событие/функцию click
+	// console.log(findButton.vm.$listeners);
+
+	// findButton.vm.$on('click', function (msg) { //кастомная установка события
+	// 	console.log(msg)
+	// })
+	// findButton.vm.$emit('click', 'hi') //кастомное возбуждение события
+
+	// await findButton.setProps({is: roundBtn})       //выбираем тип кнопки
+	// await findButton.setProps({type: String('round')}) //выбираем тип кнопки
+	// console.log(findButton.props());
+	// console.log(findButton.vm.$props);
+	// console.log(findButton.vm.$listeners.click);
+	findButton.vm.$emit('approve', wrapper.vm.$data.skill)
+
 
 	//checked
+	// await findButton.trigger('click');
 	await wrapper.vm.$nextTick(); //ожидаем асинхронного обновления всего
 	console.log(wrapper.emitted());
-	// expect(wrapper.emitted('approve')[0][0]).toEqual(wrapper.vm.skill);//событие было вызвано с определенными данными
+	expect(wrapper.emitted('approve')[0]).toEqual(wrapper.vm.$data.skill);//событие было вызвано с определенными данными
 })
